2465017c8ee91a347429cc24678be1ef75064710
Andre Alencar <aaugusto310@gmail.com>
2021-03-05 19:25:55 -0300

    implementado método que faz a validação da posição de uma peça, método que verifica se a posição é válida e método que verifica se já tem uma peça em uma determinada posição. Criação de sobrecarga no método Piece recebendo uma posição como parâmetro.

--- ChessCore/Board/Board.cs
+++ ChessCore/Board/Board.cs
@@ -18,10 +18,39 @@
             return Pieces[line, column];
         }
 
+        public Piece Piece(Position position)
+        {
+            return Pieces[position.Line, position.Column];
+        }
+
+        public bool ThereIsChessPiece(Position position)
+        {
+            ValidatePosition(position);
+            return Piece(position) != null;
+        }
+
         public void PutPiece(Piece piece, Position position)
         {
             Pieces[position.Line, position.Column] = piece;
             piece.Position = position;
+        }
+
+        public bool ValidPosition(Position position)
+        {
+            if (position.Line < 0 || position.Line >= Lines || position.Column < 0 || position.Column >= Columns)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void ValidatePosition(Position position)
+        {
+            if (!ValidPosition(position))
+            {
+                throw new BoardException("Posição Inválida!");
+            }
         }
     }
 }
