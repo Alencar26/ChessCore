277c534c8de1907d3c4b675579e3b1db803cff46
Andre Alencar <aaugusto310@gmail.com>
2021-03-05 18:21:45 -0300

    Começando a colocar as peças no tabuleiro. Implementando classes específicas das peças.

--- ChessCore/Program.cs
+++ ChessCore/Program.cs
@@ -1,4 +1,5 @@
 ﻿using board;
+using chess;
 using System;
 
 namespace ChessCore
@@ -8,6 +9,11 @@ namespace ChessCore
         static void Main(string[] args)
         {
             Board board = new Board(8,8);
+
+            board.PutPiece(new Rook(board, Color.Black), new Position(0,0));
+            board.PutPiece(new Rook(board, Color.Black), new Position(1,3));
+            board.PutPiece(new King(board, Color.Black), new Position(2,4));
+
             View.PrintBoard(board);
 
             Console.WriteLine($"");
