a4eaa0271660f9e781824d69705e764db346d5a8
Andre Alencar <aaugusto310@gmail.com>
2021-03-08 00:24:06 -0300

    refatorando a classe de View para imprimir as peças com cores distintas e acrescentando marcações em torno do tabuleiro.

--- ChessCore/Program.cs
+++ ChessCore/Program.cs
@@ -8,11 +8,24 @@ namespace ChessCore
     {
         static void Main(string[] args)
         {
-            PositionChess position = new PositionChess('b',5);
+            try
+            {
+                Board board = new Board(8, 8);
 
-            Console.WriteLine(position.ToPosition());
+                board.PutPiece(new Rook(board, Color.Black), new Position(0, 0));
+                board.PutPiece(new Rook(board, Color.White), new Position(2, 3));
+                board.PutPiece(new King(board, Color.Black), new Position(5, 1));
+                board.PutPiece(new Rook(board, Color.White), new Position(4, 4));
+                board.PutPiece(new King(board, Color.Black), new Position(4, 1));
+                board.PutPiece(new Rook(board, Color.White), new Position(2, 1));
+                board.PutPiece(new King(board, Color.Black), new Position(5, 0));
 
-            
+                View.PrintBoard(board);
+            }
+            catch (BoardException e)
+            {
+                Console.WriteLine(e.Message);
+            } 
         }
     }
 }
