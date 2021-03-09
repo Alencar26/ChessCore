ca5b8a14b4671c50a4f71fe8b2c9d51ba6689e11
Andre Alencar <aaugusto310@gmail.com>
2021-03-08 21:57:34 -0300

    implementação do método que remove uma peça de determinada posição.

--- ChessCore/Board/Board.cs
+++ ChessCore/Board/Board.cs
@@ -38,6 +38,17 @@
             piece.Position = position;
         }
 
+        public Piece RemovePiece(Position position)
+        {
+            if (Piece(position) == null)
+                return null;
+
+            Piece piece = Piece(position);
+            piece.Position = null;
+            Pieces[position.Line, position.Column] = null;
+            return piece;
+        }
+
         public bool ValidPosition(Position position)
         {
             if (position.Line < 0 || position.Line >= Lines || position.Column < 0 || position.Column >= Columns)
