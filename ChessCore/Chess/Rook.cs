a6f6b6051e5a2d59f5574d8f5343f1372f283ad3
Andre Alencar <aaugusto310@gmail.com>
2021-03-09 15:28:58 -0300

    Implementação dos movimentos válidos da Torre.

--- ChessCore/Chess/Rook.cs
+++ ChessCore/Chess/Rook.cs
@@ -10,5 +10,60 @@ namespace chess
         {
             return "T";
         }
+
+        private bool CanMove(Position position)
+        {
+            Piece piece = Board.Piece(position);
+            return piece == null || piece.Color != Color;
+        }
+
+        public override bool[,] PossibleMovements()
+        {
+            bool[,] matrix = new bool[Board.Lines, Board.Columns];
+
+            Position position = new Position(0, 0);
+
+            //up
+            position.SetValues(position.Line - 1, position.Column);
+            while (Board.ValidPosition(position) && CanMove(position))
+            {
+                matrix[position.Line, position.Column] = true;
+                if (Board.Piece(position) != null && Board.Piece(position).Color != Color)
+                    break;
+                position.Line -= 1;
+            }
+
+            //right
+            position.SetValues(position.Line, position.Column + 1);
+            while (Board.ValidPosition(position) && CanMove(position))
+            {
+                matrix[position.Line, position.Column] = true;
+                if (Board.Piece(position) != null && Board.Piece(position).Color != Color)
+                    break;
+                position.Line += 1;
+            }
+
+            //down
+            position.SetValues(position.Line + 1, position.Column);
+            while (Board.ValidPosition(position) && CanMove(position))
+            {
+                matrix[position.Line, position.Column] = true;
+                if (Board.Piece(position) != null && Board.Piece(position).Color != Color)
+                    break;
+                position.Line += 1;
+            }
+
+            //left
+            position.SetValues(position.Line, position.Column - 1);
+            while (Board.ValidPosition(position) && CanMove(position))
+            {
+                matrix[position.Line, position.Column] = true;
+                if (Board.Piece(position) != null && Board.Piece(position).Color != Color)
+                    break;
+                position.Line -= 1;
+            }
+
+            return matrix;
+        }
     }
 }
